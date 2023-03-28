e9756047d2e72c024117db096edd287a86453a9b
iyeee <1009699416@qq.com>
2023-03-28 17:49:53 +0800

    m

---
+++ gulimail-order/src/test/java/com/modq/gulimail/order/controller/OrderReturnReasonControllerTest.java
@@ -0,0 +1,28 @@
+package com.modq.gulimail.order.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OrderReturnReasonControllerTest {
+
+    @Test
+    public void list() {
+    }
+
+    @Test
+    public void info() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void delete() {
+    }
+}
